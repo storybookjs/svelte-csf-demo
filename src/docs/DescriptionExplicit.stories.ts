b4d082f88c6a81b8fbac10856668ad26516a97c9
xeho91 <xeho91@pm.me>
2024-05-13 22:24:56 +0800

    Polish

--- src/docs/DescriptionExplicit.stories.ts
+++ src/docs/DescriptionExplicit.stories.ts
@@ -1,14 +1,14 @@
-import type { Meta, StoryObj } from "@storybook/svelte";
+import type { Meta, StoryObj } from '@storybook/svelte';
 
-import DescriptionComponent from "./DescriptionComponent.svelte";
+import DescriptionComponent from './DescriptionComponent.svelte';
 
 /**
  * This description was set above the `const meta` in the regular CSF file.
  */
 const meta = {
-	title: "Description/From Comment/Regular CSF",
+	title: 'Description/From Comment/Regular CSF',
 	component: DescriptionComponent,
-	tags: ["autodocs", "!dev"],
+	tags: ['autodocs', '!dev'],
 	argTypes: {
 		useMeta: { table: { disable: true } },
 		useStory: { table: { disable: true } },
@@ -23,7 +23,7 @@ export default meta;
 
 type Story = StoryObj<DescriptionComponent>;
 
-/** This is a description for the story `JSDoc`, it was set above `export const`. */
+/** This is a description for the story `JSDoc`, it was set above `export const JSDoc`. */
 export const JSDoc: Story = { args: { useStory: true } };
 
 export const NotDescribed: Story = {};
--- src/docs/DescriptionFromComment.stories.ts
+++ src/docs/DescriptionFromComment.stories.ts
@@ -1,14 +1,14 @@
-import type { Meta, StoryObj } from "@storybook/svelte";
+import type { Meta, StoryObj } from '@storybook/svelte';
 
-import DescriptionComponent from "./DescriptionComponent.svelte";
+import DescriptionComponent from './DescriptionComponent.svelte';
 
 /**
  * This description was set above the `const meta` in the regular CSF file.
  */
 const meta = {
-	title: "Description/From Parameters/Regular CSF",
+	title: 'Description/From Parameters/Regular CSF',
 	component: DescriptionComponent,
-	tags: ["autodocs", "!dev"],
+	tags: ['autodocs', '!dev'],
 	argTypes: {
 		useMeta: { table: { disable: true } },
 		useStory: { table: { disable: true } },
@@ -19,7 +19,7 @@ const meta = {
 		docs: {
 			description: {
 				component:
-					"This description was explicitly set in the `meta.parameters`. It will override the comment above the `export const meta`.",
+					'This description was explicitly set in the `meta.parameters`. It will override the comment above the `export const meta`.',
 			},
 		},
 	},
@@ -29,14 +29,14 @@ export default meta;
 
 type Story = StoryObj<DescriptionComponent>;
 
-/** This is a description for the story `JSDoc`, it was set above `export const`. */
-export const FromStoryParameters: Story = {
+/** This is a description for the story `Parameters`, it was set above `export const Parameters`. */
+export const Parameters: Story = {
 	args: { useStory: true },
 	parameters: {
 		docs: {
 			description: {
 				story:
-					"This explicitly set description overrides the JSDoc comment above the `export const JSDoc`.",
+					'This explicitly set description overrides the JSDoc comment above the `export const Parameters`.',
 			},
 		},
 	},
