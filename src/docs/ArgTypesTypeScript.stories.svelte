885b5153efd2853346d3924f775752ec70591d97
xeho91 <xeho91@pm.me>
2024-05-16 13:47:56 +0800

    Fix invalid content for typescript svelte csf

--- src/docs/ArgTypesTypeScript.stories.svelte
+++ src/docs/ArgTypesTypeScript.stories.svelte
@@ -1,127 +1,30 @@
-<script lang="ts">
-	/**
-	 * Description for `sampleBoolean` component prop.
-	 * NOTE: The type is automatically infered.
-	 */
-	export let sampleDefaultBoolean = false;
-	/**
-	 * Description for `sampleOptionalBoolean` component prop.
-	 */
-	export let sampleOptionalBoolean: boolean | undefined;
-	/**
-	 * Description for `sampleRequiredBoolean` component prop.
-	 */
-	export let sampleRequiredBoolean: boolean;
+<script context="module" lang="ts">
+	import { Story } from '@storybook/addon-svelte-csf';
+	import type { Meta } from '@storybook/svelte';
+
+	import ArgTypesTypeScriptComponent from './ArgTypesTypeScriptComponent.svelte';
 
 	/**
-	 * Description for `sampleDefaultString` component prop.
-	 * NOTE: The type is automatically infered.
+	 * How to type `meta` for stories file using **TypeScript**.
 	 */
-	export let sampleDefaultString = 'Storybook 🤝 Svelte';
-	/**
-	 * Description for `sampleOptionalString` component prop.
-	 * @type {string | undefined}
-	 */
-	export let sampleOptionalString: string | undefined;
-	/**
-	 * Description for `sampleRequiredString` component prop.
-	 * @type {string}
-	 */
-	export let sampleRequiredString: string;
-
-	/**
-	 * Description for `sampleDefaultNumber` component prop.
-	 * NOTE: The type is automatically infered.
-	 */
-	export let sampleDefaultNumber = 9000;
-	/**
-	 * Description for `sampleOptionalNumber` component prop.
-	 */
-	export let sampleOptionalNumber: number | undefined;
-	/**
-	 * Description for `sampleRequiredNumber` component prop.
-	 */
-	export let sampleRequiredNumber: number;
-
-	/**
-	 * Description for `sampleDefaultArray` component prop.
-	 * NOTE: The type is automatically infered.
-	 */
-	export let sampleDefaultArray = ['Storybook', 'loves', 'Svelte'];
-	/**
-	 * Description for `sampleOptionalArray` component prop.
-	 */
-	export let sampleOptionalArray: string[] | undefined;
-	/**
-	 * Description for `sampleRequiredArray` component prop.
-	 */
-	export let sampleRequiredArray: string[];
-
-	type Tool = 'svelte' | 'storybook';
-	/**
-	 * Description for `sampleDefaultEnum` component prop.
-	 */
-	export let sampleDefaultEnum: Tool = 'svelte';
-	/**
-	 * Description for `sampleOptionalEnum` component prop.
-	 */
-	export let sampleOptionalEnum: Tool | undefined;
-	/**
-	 * Description for `sampleRequiredEnum` component prop.
-	 */
-	export let sampleRequiredEnum: Tool;
-
-	/**
-	 * Description for `sampleDefaultObject` component prop.
-	 * NOTE: The type is automatically infered.
-	 */
-	export let sampleDefaultObject = {
-		tool: 'svelte',
-		rating: 9000,
-	};
-	/**
-	 * Description for `sampleOptionalObject` component prop.
-	 */
-	export let sampleOptionalObject: typeof sampleDefaultObject | undefined;
-	/**
-	 * Description for `sampleRequiredObject` component prop.
-	 */
-	export let sampleRequiredObject: typeof sampleDefaultObject;
-
-	$: props = {
-		sampleDefaultBoolean,
-		sampleOptionalBoolean,
-		sampleRequiredBoolean,
-
-		sampleDefaultString,
-		sampleOptionalString,
-		sampleRequiredString,
-
-		sampleDefaultNumber,
-		sampleOptionalNumber,
-		sampleRequiredNumber,
-
-		sampleDefaultArray,
-		sampleOptionalArray,
-		sampleRequiredArray,
-
-		sampleDefaultEnum,
-		sampleOptionalEnum,
-		sampleRequiredEnum,
-
-		sampleDefaultObject,
-		sampleOptionalObject,
-		sampleRequiredObject,
-	};
+	export const meta = {
+		title: 'ArgTypes/TypeScript/Svelte CSF',
+		component: ArgTypesTypeScriptComponent,
+		tags: ['autodocs'],
+	} satisfies Meta<ArgTypesTypeScriptComponent>;
 </script>
 
-<p>
-	This Svelte component demonstrates how to type components props to fill
-	<code>ArgTypes</code> table with <strong>TypeScript</strong>.
-</p>
-
-<code>
-	{#each Object.entries(props) as [name, value]}
-		<strong>{name}</strong>: {JSON.stringify(value)}<br />
-	{/each}
-</code>
+<Story
+	name="Playground"
+	args={{
+		sampleRequiredBoolean: true,
+		sampleRequiredString: 'I agree',
+		sampleRequiredNumber: 10,
+		sampleRequiredArray: ['I', 'like', 'Svelte', 'and', 'Storybook'],
+		sampleRequiredEnum: 'storybook',
+		sampleRequiredObject: { tool: 'storybook', rating: 10 },
+	}}
+	let:args
+>
+	<ArgTypesTypeScriptComponent {...args} />
+</Story>
